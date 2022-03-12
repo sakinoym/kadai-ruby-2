3ec064b83fd1e9e2c2d5c9991191f616a89bb68d
saki.y <sakino.ym@gmail.com>
2022-03-12 06:59:00 +0000

    second

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,14 +1,16 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-    puts "Fizz"
+  if (num % 3 == 0) && (num % 5 == 0)
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else 
-    puts num
+    num
   end
 end
 
 num_max = 100
 (1..num_max).each do |num|
-  fizzbuzz(num)
+  puts fizzbuzz(num)
 end
